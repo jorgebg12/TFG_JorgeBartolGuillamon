99d142f32a7f14e802aaf20b045aaa3ac8fcfc72
jorgebg12 <79271632+jorgebg12@users.noreply.github.com>
2022-03-09 20:50:09 +0100

    salto

--- TFG_JorgeBG/Assets/Scripts/playerController2.cs
+++ TFG_JorgeBG/Assets/Scripts/playerController2.cs
@@ -9,23 +9,31 @@ public class playerController2 : MonoBehaviour
     Animator animator;
     PlayerInputActions playerInputActions;
     Rigidbody m_Rigidbody;
-
+    
     Quaternion m_Rotation = Quaternion.identity;
+
+    [SerializeField] private LayerMask groundMask;
+
+    public float groundDistance = 0.2f;
+    public Transform groundDetection; 
 
     public float speed = 2f;
     public float turnSpeed = 20f;
+    public float jumpForce;
 
     int isWalkingHas;
     int isRunningHas;
 
     bool movementPressed;
     bool runPressed;
+    bool onAir = false;
 
     public float smoothInputSpeed = .02f;
 
     Vector2 inputVector;
     Vector2 newPosition;
     Vector2 smoothInputVelocity;
+    Vector3 newDirection;
 
     void Awake()
     {
@@ -38,6 +46,14 @@ public class playerController2 : MonoBehaviour
 
         playerInputActions.characterControls.Run.performed += Run;
         playerInputActions.characterControls.Run.canceled += stop;
+
+        playerInputActions.characterControls.jump.performed += initJump;
+    }
+
+    private void initJump(InputAction.CallbackContext context)
+    {
+        if(touchGround())
+            onAir = true;
     }
 
     private void stop(InputAction.CallbackContext context)
@@ -62,7 +78,6 @@ public class playerController2 : MonoBehaviour
 
     void FixedUpdate()
     {
-        
         movement();
         rotation();
     }
@@ -73,22 +88,21 @@ public class playerController2 : MonoBehaviour
 
         //keyboard input
         inputVector = playerInputActions.characterControls.Movement.ReadValue<Vector2>();
-
+        //
         movementPressed = inputVector.x != 0 || inputVector.y != 0;
         //smooth the input
         newPosition = Vector2.SmoothDamp(newPosition, inputVector, ref smoothInputVelocity, smoothInputSpeed);
         //to vector 3
-        Vector3 newDirection = new Vector3(newPosition.x, 0, newPosition.y);
-        //
+        newDirection = new Vector3(newPosition.x, 0, newPosition.y);
         //Apply
         m_Rigidbody.MovePosition(m_Rigidbody.position + newDirection * Time.deltaTime * speed);
 
+        if (onAir) {
+            m_Rigidbody.AddForce(Vector3.up * 2 * 9.8f * jumpForce);
+            onAir = false;
+        }
 
-        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, newDirection, turnSpeed * Time.deltaTime, 0f);
-        m_Rotation = Quaternion.LookRotation(desiredForward);
-        m_Rigidbody.MoveRotation(m_Rotation);
-
-
+        //Detect animation to play
         if (movementPressed && !isWalking)
         {
             animator.SetBool(isWalkingHas, true);
@@ -106,18 +120,15 @@ public class playerController2 : MonoBehaviour
             animator.SetBool(isRunningHas, false);
         }
     }
+    private bool touchGround()
+    {
+        //Check if the player is touching ground
+        return Physics.CheckSphere(groundDetection.position, groundDistance, groundMask); ;
+    }
     void rotation()
     {
-        /*
-        Vector3 currentPosition = transform.position;
-        Vector3 newPos = new Vector3(newPosition.x, 0, newPosition.y);
-        Vector3 positionToLook = currentPosition + newPos;
-        transform.LookAt(positionToLook);*/
-
-        m_Rotation = transform.rotation;
-        Vector3 newPos = new Vector3(newPosition.x, 0, newPosition.y);
-        if(newPos != Vector3.zero)
-            m_Rotation = Quaternion.LookRotation(newPos);
-        transform.rotation = Quaternion.Slerp(transform.rotation, m_Rotation, Time.deltaTime * turnSpeed);
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, newDirection, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+        m_Rigidbody.MoveRotation(m_Rotation);
     }
 }
